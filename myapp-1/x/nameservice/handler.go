791e021cffede0d7755ddc33c1c32d20376eec98
Aziz Yano <32180132+azizyano@users.noreply.github.com>
2020-04-21 00:52:41 +0100

    step N17

--- myapp-1/x/nameservice/handler.go
+++ myapp-1/x/nameservice/handler.go
@@ -6,7 +6,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
-
+/*
 // NewHandler creates an sdk.Handler for all the nameservice type messages
 func NewHandler(k Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
@@ -41,4 +41,88 @@ func handleMsg<Action>(ctx sdk.Context, k Keeper, msg Msg<Action>) (*sdk.Result,
 	)
 
 	return &sdk.Result{Events: ctx.EventManager().Events()}, nil
+}
+*/
+
+// NewHandler returns a handler for "nameservice" type messages.
+func NewHandler(keeper Keeper) sdk.Handler {
+	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		switch msg := msg.(type) {
+		case MsgSetName:
+			return handleMsgSetName(ctx, keeper, msg)
+		default:
+			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, fmt.Sprintf("Unrecognized nameservice Msg type: %v", msg.Type()))
+		}
+	}
+}
+
+// Handle a message to set name
+func handleMsgSetName(ctx sdk.Context, keeper Keeper, msg MsgSetName) (*sdk.Result, error) {
+	if !msg.Owner.Equals(keeper.GetOwner(ctx, msg.Name)) { // Checks if the the msg sender is the same as the current owner
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, &quot;Incorrect Owner&quot;) // If not, throw an error
+	}
+	keeper.SetName(ctx, msg.Name, msg.Value) // If so, set the name to the value specified in the msg.
+	return &amp;sdk.Result{}, nil                // return
+}
+// NewHandler returns a handler for &quot;nameservice&quot; type messages.
+func NewHandler(keeper Keeper) sdk.Handler {
+	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		switch msg := msg.(type) {
+		case MsgSetName:
+			return handleMsgSetName(ctx, keeper, msg)
+		case MsgBuyName:
+			return handleMsgBuyName(ctx, keeper, msg)
+		default:
+			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, fmt.Sprintf(&quot;Unrecognized nameservice Msg type: %v&quot;, msg.Type()))
+		}
+	}
+}
+// Handle a message to buy name
+func handleMsgBuyName(ctx sdk.Context, keeper Keeper, msg MsgBuyName) (*sdk.Result, error) {
+	// Checks if the the bid price is greater than the price paid by the current owner
+	if keeper.GetPrice(ctx, msg.Name).IsAllGT(msg.Bid) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, &quot;Bid not high enough&quot;) // If not, throw an error
+	}
+	if keeper.HasOwner(ctx, msg.Name) {
+		err := keeper.CoinKeeper.SendCoins(ctx, msg.Buyer, keeper.GetOwner(ctx, msg.Name), msg.Bid)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		_, err := keeper.CoinKeeper.SubtractCoins(ctx, msg.Buyer, msg.Bid) // If so, deduct the Bid amount from the sender
+		if err != nil {
+			return nil, err
+		}
+	}
+	keeper.SetOwner(ctx, msg.Name, msg.Buyer)
+	keeper.SetPrice(ctx, msg.Name, msg.Bid)
+	return &amp;sdk.Result{}, nil
+}
+// NewHandler returns a handler for &quot;nameservice&quot; type messages.
+func NewHandler(keeper Keeper) sdk.Handler {
+	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		switch msg := msg.(type) {
+		case MsgSetName:
+			return handleMsgSetName(ctx, keeper, msg)
+		case MsgBuyName:
+			return handleMsgBuyName(ctx, keeper, msg)
+		case MsgDeleteName:
+			return handleMsgDeleteName(ctx, keeper, msg)
+		default:
+			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, fmt.Sprintf(&quot;Unrecognized nameservice Msg type: %v&quot;, msg.Type()))
+		}
+	}
+}
+// Handle a message to delete name
+// Handle a message to delete name
+func handleMsgDeleteName(ctx sdk.Context, keeper Keeper, msg MsgDeleteName) (*sdk.Result, error) {
+	if !keeper.IsNamePresent(ctx, msg.Name) {
+		return nil, sdkerrors.Wrap(types.ErrNameDoesNotExist, msg.Name)
+	}
+	if !msg.Owner.Equals(keeper.GetOwner(ctx, msg.Name)) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, &quot;Incorrect Owner&quot;)
+	}
+
+	keeper.DeleteWhois(ctx, msg.Name)
+	return &amp;sdk.Result{}, nil
 }
